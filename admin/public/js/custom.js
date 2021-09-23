943199d18a95b620dfd9a0ef132f7343fed29dfd
AliSami6 <alisamicse320@gmail.com>
2021-09-23 23:32:14 +0600

    portfolio website
    portfolio website blade

---
+++ admin/public/js/custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+$('#VisitorDt').DataTable();
+$('.dataTables_length').addClass('bs-select');
+});
+
+
+function getServiceData(){
+	axios.post('/getServiceData')
+  .then(function (response) {
+  	if (response.status == 200){
+
+  		$('#mainDiv').removeClass('d-none');
+        $('#loaderDiv').addClass('d-none');
+
+  		var dataJSON=response.data;
+	    console.log(dataJSON)
+	    $.each(dataJSON, function(i, item) {
+	    $('<tr>').html(
+	      "<td>" + dataJSON[i].service_name +"</td>"+
+	      "<td>" + dataJSON[i].service_des +"</td>"+
+	      "<td>" + dataJSON[i].service_img +"</td>"+
+	      "<td><a href=''><i class='fas fa-edit'></i></a></td>"+
+	      "<td><a href=''><i class='fas fa-trash-alt'></i></a></td>"
+	      ).appendTo('#servicetable');
+	   });
+  	}else{
+
+  		$('#loaderDiv').addClass('d-none');
+        $('#WrongDiv').removeClass('d-none');
+  	}
+	    
+
+}).catch(function (error) {
+	
+  $('#loaderDiv').addClass('d-none');
+  $('#WrongDiv').removeClass('d-none');
+});
+
+}
---
+++ site/app/Http/Controllers/HomeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Visitor;
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $UserIP=$_SERVER['REMOTE_ADDR'];
+        date_default_timezone_set("Asia/Dhaka");
+        $timeDate= date("Y-m-d h:i:sa");
+        Visitor::insert(['ip_address'=>$UserIP,'visit_time'=>$timeDate]);
+        $ServicesData= json_decode( Service::all());
+        return view('Home',[
+            'ServicesData'=>$ServicesData,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Home  $home
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Home $home)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Home  $home
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Home $home)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Home  $home
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Home $home)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Home  $home
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Home $home)
+    {
+        //
+    }
+}
